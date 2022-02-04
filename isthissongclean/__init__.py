1068757474aa8fe823c68de71fa3d2ae420402c4
Tim Ogden <togden1999@gmail.com>
2022-02-04 14:32:21 -0500

    First commit

---
+++ isthissongclean/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+from markupsafe import escape
+
+import os
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'isthissongclean.sqlite')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def hello_world():
+        return 'Hello world'
+
+    return app
